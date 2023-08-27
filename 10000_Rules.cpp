4b01f501690742b47476acd165a4091052855383
Tucker <tuckershaw79@gmail.com>
2023-08-27 11:26:53 -0700

    Outlined game rules

---
+++ 10000_Rules.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/*
+ * 10,000 Dice Game Rules: (2+ players)
+ 
+ * To start the game:
+    * Each player rolls a die to determine who rolls first
+
+ * Playing the game:
+    * Player rolls 6 dice
+        * Check for single 1s and 5s, three or more-of-a-kinds, or straights
+            * single 1 = 100pts, single 5 = 50pts
+            * three-of-a-kind = num*100pts (1s are exception = 1000pts) doubling with 4-of-a-kind and so on
+            * straight = 1200pts, three pairs = 600pts
+        * Set aside scoring dice
+        * End turn by adding pts or a roll that scored 0 pts
+            * To earn continuing pts, the player MUST roll 1000pts on one round
+            * If all dice have been rolled on a turn and they score pts for the player, the player MUST roll all 6 at least one more time
+* To win the game:
+    * First player to score 10,000pts wins. Each other player gets one extra attempt to beat that player's score if possible
+        * Keep a track of what all players score
+*/
+
+int main()
+{
+    return 0;
+}
