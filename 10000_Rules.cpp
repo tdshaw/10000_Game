33c8045c92733ddc8fd35c1fbd036f364ec90c8c
Tucker <tuckershaw79@gmail.com>
2023-09-02 14:13:13 -0700

    Backend: Determining Order
    Added a set of recursive functions that will determine the order that the players will start based on their initial dice rolls.

--- 10000_Rules.cpp
+++ 10000_Rules.cpp
@@ -23,9 +23,24 @@
         * Keep a track of what all players score
 */
 
+const int CAPACITY = 10;
+
+struct FirstRoll
+{
+    int indices[CAPACITY];
+    int instances = 0;
+};
+
 int determineRoll();
+void rollAll(int ar[], const int size);
 void getPlayerNames(Player ar[], const int num_players);
 void getPlayerTurns(Player ar[], const int num_players);
+void getPlayerTurnsRecursive(Player ar[], int indices[], int num_players, int& turn);
+void swapPlayers(Player& p1, Player& p2);
+int partition(Player ar[], int start, int end);
+void sortPlayers(Player ar[], int start, int end);
+
+void rollSome(const int ar1[], int ar2[], const int size1, const int size2);
 
 int main()
 {
@@ -33,8 +48,14 @@ int main()
 
     int num_players, roll, high_roll = 0, index;
 
-    std::cout << "Welcome to the dice game 10,000! \nHow many players? Enter a number (2-6): ";
+    std::cout << "Welcome to the dice game 10,000! \nHow many players? Enter a number (2-10): ";
     std::cin >> num_players;
+
+    while(num_players < 2 || num_players > 10)
+    {
+      std::cout << "Invalid input. Enter a number (2-10): ";  
+      std::cin >> num_players;
+    }
 
     Player players[num_players];
 
@@ -46,12 +67,44 @@ int main()
 
     getPlayerTurns(players, num_players);
 
+    sortPlayers(players, 0, num_players - 1);
+
+    std::cout << players[0].getName() << " will roll first. Turn#" << players[0].getTurn() << std::endl;
+
     return 0;
 }
 
 int determineRoll()
 {
     return rand() % 6 + 1;
+}
+
+void rollAll(int ar[], const int size)
+{
+    int roll;
+
+    for(int i = 0; i < size; i++)
+    {
+        roll = determineRoll();
+        ar[i] = roll;
+        std::cout << i << ": " << roll << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void rollSome(const int ar1[], int ar2[], const int size1, const int size2)
+{
+    for(int i = 0; i < size1; i++)
+        for(int j = 0; j < size2; j++)
+            ar2[ar1[i]] = determineRoll();
+}
+
+void findAndRemove(int ar1[], const int idx, int& size)
+{
+    for(int i = idx; i < size - 1; i++)
+        ar1[i] = ar1[i + 1];
+
+    size--;
 }
 
 void getPlayerNames(Player ar[], const int num_players)
@@ -66,29 +119,127 @@ std::string input;
     }
 }
 
+/*
+ * Everyone rolls once
+    * Highest roll goes first, etc (highest -> turn = 0, lowest -> turn = num_players - 1)
+    * Check for duplicates
+        * Repeat steps
+
+ * Recursive Steps:
+    * Start from 6 -> 1 
+    * Base Case: # rolled once
+        * Add turn to player
+    * Case: Duplicate rolls
+        * Re-roll duplicates  
+*/
+
 void getPlayerTurns(Player ar[], const int num_players)
 {
-    int roll, prev_roll, index, high_roll = 0;
-    int incomplete[num_players];
-    bool done = true;
+    int all_rolls[num_players];
+    int dupe_rolls[num_players];
+    int dupe_indices[num_players];
+    int num_instances, highest_roll, highest_idx, turn = 0;
+    int roll;
+    bool done;
 
-    do {
+    std::cout << "Initial roll..." << std::endl;
+
+    for(int i = 0; i < num_players; i++)
+    {
+        roll = determineRoll();
+        all_rolls[i] = roll;
+
+        std::cout << ar[i].getName() << " rolled a " << roll << std::endl; 
+    }
+    std::cout << std::endl;
+
+    for(int i = 6; i > 0; i--)
+    {
+        num_instances = 0;
+
+        for(int j = 0; j < num_players; j++)
+            if(!ar[j].getFlag() && all_rolls[j] == i)
+               dupe_indices[num_instances++] = j;
+    
+        std::cout << "Dupes for " << i << "'s" << std::endl;
+        getPlayerTurnsRecursive(ar, dupe_indices, num_instances, turn);
+    }
+}
+
+void getPlayerTurnsRecursive(Player ar[], int indices[], int num_players, int& turn)
+{
+    if(num_players == 0)
+        return;
+    else if(num_players == 1)
+        ar[indices[0]].setTurn(turn++);
+    else
+    {
+        int rolls[num_players];
+        int dupe_indices[num_players];
+        int num_instances, roll;
+
+        std::cout << "Dupes roll..." << std::endl;
         for(int i = 0; i < num_players; i++)
         {
             roll = determineRoll();
-
-            ar[i].setTurn(roll);
-
-            if(roll > high_roll)
-            {
-                high_roll = roll;
-                index = i;
-            }
-
-            std::cout << ar[i].getName() << " rolled a " << roll << std::endl;
+            rolls[i] = roll;
+            std::cout << ar[indices[i]].getName() << " rolled a " << roll << std::endl; 
         }
+        std::cout << std::endl;
 
-    } while(!done);
+        for(int i = 6; i > 0; i--)
+        {
+            num_instances = 0;
 
-    std::cout << ar[index].getName() << " gets to start." << std::endl;
+            for(int j = 0; j < num_players; j++)
+                if(rolls[j] == i)
+                    dupe_indices[num_instances++] = indices[j];
+
+            std::cout << "Dupes for " << i << "'s" << std::endl;
+            getPlayerTurnsRecursive(ar, dupe_indices, num_instances, turn);
+        }
+    }
+}
+
+void swapPlayers(Player& p1, Player& p2)
+{
+    Player temp = p1;
+    p1 = p2;
+    p2 = temp;
+}
+
+int partition(Player ar[], int start, int end)
+{
+    int pivot = (start + end) / 2;
+    int swap_index = start;
+
+    swapPlayers(ar[pivot], ar[end]);
+
+    pivot = end;
+
+    for(int i = start; i < end; i++)
+    {
+        if(ar[i] <= ar[pivot])
+        {
+            swapPlayers(ar[i], ar[swap_index]);
+
+            swap_index++;
+        }
+    }
+
+    swapPlayers(ar[swap_index], ar[pivot]);
+
+    return swap_index;
+}
+
+void sortPlayers(Player ar[], int start, int end)
+{
+    if(start < end)
+    {
+        int pivot = partition(ar, start, end);
+
+        sortPlayers(ar, start, pivot - 1);
+
+        sortPlayers(ar, pivot + 1, end);
+    }
 }
--- player.h
+++ player.h
@@ -1,3 +1,5 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <string>
 
 class Player
@@ -5,10 +7,34 @@ class Player
     std::string name = "";
     int turn = -1;
     int points = 0;
+    bool flag = false;
 
     public:
     void setName(const std::string input) { this->name = input; };
     const std::string getName() { return this->name; };
-    void setTurn(const int input) { this->turn = input; };
+    void setFlag(bool input) { this->flag = input; };
+    const bool getFlag() { return this->flag; };
+    void setTurn(int input) { this->turn = input; this->setFlag(true); };
     const int getTurn() { return this->turn; };
+    void operator =(Player& p);
+    bool operator <(const Player p) { return (this->turn < p.turn) ? true:false;};
+    bool operator <=(const Player p) { return (this->turn <= p.turn) ? true:false; };
 };
+
+void Player::operator =(Player& p) 
+{ 
+    Player temp; 
+    temp.name = p.name; 
+    temp.turn = p.turn; 
+    temp.points = p.points;
+
+    p.name = this->name;
+    p.turn = this->turn;
+    p.points = this->points;
+
+    this->name = temp.name;
+    this->turn = temp.turn;
+    this->points = temp.points; 
+}
+
+#endif
